3c6ebb3f2965430babb9061c6c9d9e1d5a1a40db
alailayangan27-png <alailayangan27@gmail.com>
2026-03-19 22:15:29 +0700

    Update mint.js

--- api/mint.js
+++ api/mint.js
@@ -1,1 +1,38 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+
+  const { wallet, amount } = req.body;
+
+  if (!wallet) {
+    return res.json({ message: "Enter wallet" });
+  }
+
+  // ambil total mint
+  const { data } = await supabase
+    .from('mint')
+    .select('amount');
+
+  let total = 0;
+  data.forEach(d => total += d.amount);
+
+  if (total + amount > 10000) {
+    return res.json({ message: "Mint full" });
+  }
+
+  // simpan
+  await supabase.from('mint').insert({
+    wallet,
+    amount,
+    status: "pending"
+  });
+
+  res.json({
+    message: `Send $${amount} USDC to project wallet`
+  });
+}
